e08bf9852cf7dbded2a0c52693f08a5cb447a95b
Darssh <darshitsuthar91@gmail.com>
2016-11-15 11:37:14 -0800

    Refactor code

--- functional_tests.py
+++ functional_tests.py
@@ -12,8 +12,15 @@ class NewVisitorTest(unittest.TestCase):
 		))
 		self.browser.implicitly_wait(3)
 
+
 	def tearDown(self):
 		self.browser.quit()
+
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
@@ -33,18 +40,15 @@ class NewVisitorTest(unittest.TestCase):
 
 		inputbox.send_keys(Keys.ENTER)
 		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers' ,[row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('2: Use peacock feathers to make a fly' ,[row.text for row in rows])
-				
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
