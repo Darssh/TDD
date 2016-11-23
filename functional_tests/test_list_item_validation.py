64fae056e8ff3196d20d259ea5851475bfc7e9cd
Darssh <darshitsuthar91@gmail.com>
2016-11-23 15:03:47 -0800

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,18 +8,28 @@ class ItemValidationTest(FunctionalTest):
 		self.browser.get(self.server_url)
 		self.get_item_input_box().send_keys('\n')
 
-		error = self.browser.find_element_by_css_selector('.has-error')
-		self.assertEqual(error.text, "You can't have an empty list item")
-
 		self.get_item_input_box().send_keys('Buy milk\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 
 		self.get_item_input_box().send_keys('\n')
 
 		self.check_for_row_in_list_table('1: Buy milk')
-		error = self.browser.find_element_by_css_selector('.has-error')
-		self.assertEqual(error.text, "You can't have an empty list item")
 
 		self.get_item_input_box().send_keys('Make tea\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 		self.check_for_row_in_list_table('2: Make tea')
+
+
+	def test_cannot_add_duplicate_items(self):
+	    # Edith goes to the home page and starts a new list
+	    self.browser.get(self.server_url)
+	    self.get_item_input_box().send_keys('Buy wellies\n')
+	    self.check_for_row_in_list_table('1: Buy wellies')
+
+	    # She accidentally tries to enter a duplicate item
+	    self.get_item_input_box().send_keys('Buy wellies\n')
+
+	    # She sees a helpful error message
+	    self.check_for_row_in_list_table('1: Buy wellies')
+	    error = self.browser.find_element_by_css_selector('.has-error')
+	    self.assertEqual(error.text, "You've already got this in your list")
